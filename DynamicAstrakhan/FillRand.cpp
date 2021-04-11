ce43b94d006b0c118002a0d3b2ce55d237f33043
Clayman <Clayman@DESKTOP-DM8E77U>
2021-04-11 19:48:51 +0300

    Separation DONE.

---
+++ DynamicAstrakhan/FillRand.cpp
@@ -0,0 +1,19 @@
+//Реализации функций выносятся в *.cpp файлы.
+#include"FillRand.h"
+void FillRand(int arr[], const int n)//Реализация функции - Определение функции (Function definition)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void FillRand(int** arr, const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
---
+++ DynamicAstrakhan/FillRand.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<stdlib.h>
+
+void FillRand(int arr[], const int n);//Прототип функции - объявление функции(Function declaration)
+void FillRand(int** arr, const int m, const int n);
